5efe965c182d6b2e35eb4a270b845074f8d5fa52
MarceleDutra Cardozo <marcelecardozo@gmail.com>
2023-06-28 20:40:08 -0300

    Ajustando o conteúdo do modal e dos cards.

--- js/home.js
+++ js/home.js
@@ -14,7 +14,7 @@ function renderVideos() {
 
   for (const category in categories) {
     html += `
-    <div class="col-12 pt-2" 
+    <div class="col-12 pt-3" 
       <p>
         <a href="" class="link-title fw-bold">${category}
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
@@ -31,15 +31,13 @@ function renderVideos() {
       <div class="card" style="width: 18rem" onmouseenter="showDetail(this)" onmouseleave="closeDetail(this)">
         <img src="${item.img}" class="card-img-top img-fluid" alt="..." />
         <div class="card-body">
-        <a href="#" class="custom-link video-link" data-bs-toggle="modal" data-bs-target="#videoModal" onclick="openVideoModal(event, '${item.link}')"> 
-            <p class="card-title detail-video">
-              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+            <p class="custom-link video-link card-title detail-video" data-bs-toggle="modal" data-bs-target="#videoModal" onclick="openVideoModal(event, '${item.link}')">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
               </svg>
               <span>${item.title}</span>
            </p>
-          </a>
         </div>
        </div>
     </div>
